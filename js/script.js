d38f441c9133faf09a52476c39e0f8fa4ffefcb3
matador7495 <id4acc2014@gmail.com>
2023-10-23 15:47:37 +0330

    services section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-   header.classList.toggle("sticky", window.scrollY > 0);
+   header.classList.toggle("sticky", window.scrollY > 120);
 });
