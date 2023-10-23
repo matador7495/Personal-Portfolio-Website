951c68678f6a10432a31f99d4d84cc35c0a79c86
matador7495 <id4acc2014@gmail.com>
2023-10-23 20:09:21 +0330

    Responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const header = document.querySelector("header");
-
 window.addEventListener("scroll", () => {
    header.classList.toggle("sticky", window.scrollY > 120);
 });
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+menu.onclick = () => {
+   menu.classList.toggle("bx-x");
+   navlist.classList.toggle("active");
+};
+window.onscroll = () => {
+   menu.classList.remove("bx-x");
+   navlist.classList.remove("active");
+};
